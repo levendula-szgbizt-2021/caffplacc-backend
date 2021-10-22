30e77cc6318f247126ff1b718dd80b363fdbfd60
bzp99 <bertalan.peter@bp99.eu>
2021-10-22 16:21:23 +0200

    bootstrap using spring-boot-cli
    Here is the exact command used:
    
    spring init -x \
        -a caffplacc -g hu.bme.szgbizt.levendula \
        -j 11 --build gradle \

---
+++ src/test/java/hu/bme/szgbizt/levendula/caffplacc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.szgbizt.levendula.caffplacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
