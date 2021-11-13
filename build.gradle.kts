94e642a3dda41df54e4a6f11f96ae207f38dc9ee
bzp99 <bertalan.peter@bp99.eu>
2021-11-13 12:38:36 +0100

    add maven pom.xml publishing task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,44 @@
 plugins {
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("org.springframework.boot") version "2.5.6"
-	java
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.5.6"
+    java
+    `maven-publish`
 }
 
 group = "hu.bme.szgbizt.levendula"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+
+// use JUnit5 for all test tasks
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+// configure publishing to maven
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+
+}
+tasks.withType<GenerateMavenPom>().configureEach {
+    destination = layout.projectDirectory.file("pom.xml").getAsFile()
+}
+tasks.register("pom") {
+    dependsOn("generatePomFileForMavenPublication")
 }
