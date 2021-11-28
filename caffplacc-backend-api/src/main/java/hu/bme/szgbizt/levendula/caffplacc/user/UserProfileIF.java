7ebd3f303e0ffd9d74cc4141595cb65606765965
Radványi Ákos <radvanyi.akos@edu.bme.hu>
2021-11-28 20:21:46 +0100

    swagger fix

--- caffplacc-backend-api/src/main/java/hu/bme/szgbizt/levendula/caffplacc/user/UserProfileIF.java
+++ caffplacc-backend-api/src/main/java/hu/bme/szgbizt/levendula/caffplacc/user/UserProfileIF.java
@@ -4,13 +4,14 @@ import hu.bme.szgbizt.levendula.caffplacc.exception.CaffplaccException;
 import hu.bme.szgbizt.levendula.caffplacc.login.UserDto;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserProfileIF {
 
     @ApiOperation(value = "getUserData", produces = MediaType.APPLICATION_JSON_VALUE, notes = "This endpoint can be called by an user to get their user data.")
-    @PostMapping
+    @GetMapping
     UserResponse getUserData() throws CaffplaccException;
 
     @ApiOperation(value = "changeUserData", produces = MediaType.APPLICATION_JSON_VALUE, notes = "This endpoint can be called by an user to update their user data.")
