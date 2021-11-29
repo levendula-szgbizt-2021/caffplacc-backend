d60a975ef775881c1fd7dbf6c937d07f8084677b
Radványi Ákos <31032744+Radakxx@users.noreply.github.com>
2021-11-29 11:59:00 +0100

    api fix

--- caffplacc-backend-core/src/main/java/hu/bme/szgbizt/levendula/caffplacc/presentation/UserController.java
+++ caffplacc-backend-core/src/main/java/hu/bme/szgbizt/levendula/caffplacc/presentation/UserController.java
@@ -6,10 +6,7 @@ import hu.bme.szgbizt.levendula.caffplacc.service.UserService;
 import hu.bme.szgbizt.levendula.caffplacc.user.UserProfileIF;
 import hu.bme.szgbizt.levendula.caffplacc.user.UserResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @PreAuthorize("hasRole('ROLE_USER')")
@@ -35,6 +32,7 @@ public class UserController implements UserProfileIF {
     }
 
     @Override
+    @DeleteMapping
     public void deleteUserData() throws CaffplaccException {
         service.deleteUserData();
     }
