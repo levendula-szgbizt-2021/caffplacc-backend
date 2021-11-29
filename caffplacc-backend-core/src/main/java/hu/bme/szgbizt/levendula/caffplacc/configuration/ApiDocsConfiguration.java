dea6ff3401fecfdf32a9e32d1ff2eabf0de249f4
Radványi Ákos <31032744+Radakxx@users.noreply.github.com>
2021-11-29 13:21:34 +0100

    paging fix

--- caffplacc-backend-core/src/main/java/hu/bme/szgbizt/levendula/caffplacc/configuration/ApiDocsConfiguration.java
+++ caffplacc-backend-core/src/main/java/hu/bme/szgbizt/levendula/caffplacc/configuration/ApiDocsConfiguration.java
@@ -1,8 +1,10 @@
 package hu.bme.szgbizt.levendula.caffplacc.configuration;
 
+import hu.bme.szgbizt.levendula.caffplacc.paging.SwaggerPageable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,6 +16,7 @@ public class ApiDocsConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .directModelSubstitute(Pageable.class, SwaggerPageable.class)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("hu.bme.szgbizt.levendula.caffplacc"))
                 .paths(PathSelectors.any())
